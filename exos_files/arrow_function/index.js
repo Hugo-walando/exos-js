2c0251adffacd7e7c06f039b906b52827b0b0b18
Hugo-walando <hugowalando@gmail.com>
2024-05-15 12:08:13 +0200

    11. arrow_function done

---
+++ exos_files/arrow_function/index.js
@@ -0,0 +1,31 @@
+// Rewrite with arrow functions
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function () {
+    alert("You agreed.");
+  },
+  function () {
+    alert("You canceled the execution.");
+  }
+);
+
+const ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+};
+
+ask(
+  "Do you agree?",
+  () => {
+    alert("You agreed.");
+  },
+  () => {
+    alert("You canceled the execution.");
+  }
+);
