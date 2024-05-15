6bdd1dbcc9ee11fab07461e9a70b1d0c7f154a57
Hugo-walando <hugowalando@gmail.com>
2024-05-15 09:35:48 +0200

    1.  variables done

---
+++ exos_files/variables/index.js
@@ -0,0 +1,20 @@
+// Working with variables
+
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+console.log(admin);
+
+// Giving the right name
+
+let ourPlanetName;
+let currentUserName;
+
+// Uppercase const?
+
+const BIRTHDAY = "18.04.1982";
+
+const age = someCode(BIRTHDAY);
